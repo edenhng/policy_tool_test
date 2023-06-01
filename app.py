25baa22c143b7dc87f3151c7543403e1a18c64ee
Bao Eden Hoang <90878235+edenhng@users.noreply.github.com>
2023-06-01 16:04:17 +0930

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from annotated_text import annotated_text
 
 @st.cache(show_spinner=False, allow_output_mutation=True, suppress_st_warning=True)
 def load_models():
-    french_model = spacy.load("./models/fr/")
-    english_model = spacy.load("./models/en/")
+    french_model = spacy.load("fr_core_news_sm")
+    english_model = spacy.load("en_core_web_sm")
     models = {"en": english_model, "fr": french_model}
     return models
 
